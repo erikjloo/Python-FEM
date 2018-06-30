fda8d1e13c780215d8108c0485d8af73ebf2b9a6
Erik <erik.giesenloo@gmail.com>
2018-06-30 15:12:41 +0200

    v2.1

--- globalData.py
+++ globalData.py
@@ -6,7 +6,7 @@ from constraints import Constraints
 from properties import Properties
 from loadTable import LoadTable
 from algebra import MatrixBuilder
-from models import ModelFactory
+# from models import ModelFactory
 from mesh import Mesh
 
 class GlobalData(Properties):
@@ -33,31 +33,7 @@ class GlobalData(Properties):
     """
 
     def __init__(self, props):
+        self.properties = {}
         self.i = 0
         self.ndof = 0
-        self.mesh = Mesh()
-        self.fext = np.zeros(0)
-        self.fint = np.zeros(0)
-        self.disp = np.zeros(0)
-        self.load = LoadTable()
-        self.cons = Constraints()
-        self.mbuild = MatrixBuilder(0)
     
-    def makeMesh(self, props, conf):
-        self.mesh.initialize(props, conf)
-
-    def makeModel(self, props, conf):
-        self.model = ModelFactory("model", conf, props, self.mesh)
-        self.ndof = self.mesh.dofCount()
-
-    def makeConstraints(self, props):
-        # self.set("cons",self.cons)
-        self.cons.initialize(props, self.mesh)
-
-    def makeMatrixBuilder(self):
-        self.mbuild.resize(self.ndof)
-
-    def makeVectors(self):
-        self.fext.resize(self.ndof)
-        self.fint.resize(self.ndof)
-        self.disp.resize(self.ndof)
